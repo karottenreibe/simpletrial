9841e222857a889461b3de6f350f4582b329a09c
Fabian Streitel <streitel@cqse.eu>
2017-02-20 21:22:20 +0100

    initial commit

---
+++ simpletrial/src/main/java/be/rottenrei/simpletrial/SimpleTrial.java
@@ -0,0 +1,78 @@
+package be.rottenrei.simpletrial;
+
+
+import android.app.backup.BackupManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+
+import java.util.Date;
+
+public class SimpleTrial {
+
+    public static final String DEFAULT_SHARED_PREFERENCES_FILE = "trial_repo";
+    public static final String DEFAULT_PREFERENCE_NAME = "trial_start";
+
+    private static final long NOT_AVAILABLE_TIMESTAMP = -1;
+
+    private final Context context;
+    private final String sharedPreferencesFile;
+    private final String preferenceName;
+    private final long trialDurationInMilliseconds;
+
+    private long trialStartTimestamp;
+
+    public SimpleTrial(Context context, String sharedPreferencesFile, String preferenceName,
+            long trialDurationInDays) {
+        this.context = context;
+        this.sharedPreferencesFile = sharedPreferencesFile;
+        this.preferenceName = preferenceName;
+        this.trialDurationInMilliseconds = trialDurationInDays * 24L * 3600 * 1000;
+        trialStartTimestamp = getTrialStartTimestamp();
+        persistTrialStartTimestamp();
+        BackupManager backupManager = new BackupManager(context);
+        backupManager.dataChanged();
+    }
+
+    public SimpleTrial(Context context, long trialDurationInDays) {
+        this(context, DEFAULT_SHARED_PREFERENCES_FILE, DEFAULT_PREFERENCE_NAME,
+                trialDurationInDays);
+    }
+
+    public boolean isTrialPeriodFinished() {
+        return new Date().getTime() >= trialStartTimestamp + trialDurationInMilliseconds;
+    }
+
+    private void persistTrialStartTimestamp() {
+        SharedPreferences preferences = context
+                .getSharedPreferences(sharedPreferencesFile, Context.MODE_PRIVATE);
+        preferences.edit().putLong(preferenceName, trialStartTimestamp).apply();
+    }
+
+    private long getTrialStartTimestamp() {
+        long packageManagerTimestamp = getPackageManagerTimestamp();
+        long preferencesTimestamp = getPreferencesTimestamp();
+
+        if (preferencesTimestamp == NOT_AVAILABLE_TIMESTAMP) {
+            return packageManagerTimestamp;
+        }
+        return Math.min(packageManagerTimestamp, preferencesTimestamp);
+    }
+
+    private long getPackageManagerTimestamp() {
+        try {
+            return context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0).firstInstallTime;
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException("Your own package " + context.getPackageName() +
+                    " is not availbable. This should never happen", e);
+        }
+    }
+
+    private long getPreferencesTimestamp() {
+        SharedPreferences preferences = context
+                .getSharedPreferences(sharedPreferencesFile, Context.MODE_PRIVATE);
+        return preferences.getLong(preferenceName, NOT_AVAILABLE_TIMESTAMP);
+    }
+
+}
